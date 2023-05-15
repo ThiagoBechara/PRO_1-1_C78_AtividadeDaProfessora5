d8a4588de6ceff39f7b423c9addf3985625395cf
ThiagoBechara <95245669+ThiagoBechara@users.noreply.github.com>
2023-05-15 17:55:17 -0300

    Update IssLocation.js

--- screens/IssLocation.js
+++ screens/IssLocation.js
@@ -55,7 +55,7 @@ export default class IssLocationScreen extends Component {
             return (
                 <View style={styles.container}>
                     <SafeAreaView style={styles.droidSafeArea} />
-                    <ImageBackground source={require('../assets/iss_bg.jpg')} style={styles.backgroundImage}>
+                    <ImageBackground source={require('../assets/bg.pnrg')} style={styles.backgroundImage}>
                         <View style={styles.titleContainer}>
                             <Text style={styles.titleText}>Localização da EEI</Text>
                         </View>
@@ -75,6 +75,12 @@ export default class IssLocationScreen extends Component {
                                     <Image source={require('../assets/iss_icon.png')} style={{ height: 50, width: 50 }} />
                                 </Marker>
                             </MapView>
+                        </View>
+                        <View style={styles.infoContainer}>
+                        <Text style={styles.infoText}>Latitude: {this.state.location.latitude}</Text>
+                        <Text style={styles.infoText}>Longitude: {this.state.location.longitude}</Text>
+                        <Text style={styles.infoText}>Altitude: {this.state.location.altitude}</Text>
+                        <Text style={styles.infoText}>Velocidade: {this.state.location.velocity}</Text>
                         </View>
                     </ImageBackground>
                 </View>
@@ -115,5 +121,19 @@ const styles = StyleSheet.create({
     map: {
         width: "100%",
         height: "100%"
+    },
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: "white",
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: 700
     }
+
 });
